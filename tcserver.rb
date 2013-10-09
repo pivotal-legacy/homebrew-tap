7e8abbe5dd2f1cb0f3aff2c8703cbb82a2777a52
Kashyap Parikh <parikhkc@gmail.com>
2013-10-09 00:30:42 -0700

    Add user input for ofac and license terms

--- tcserver.rb
+++ tcserver.rb
@@ -12,6 +12,24 @@ class Tcserver < Formula
   skip_clean 'libexec/templates/base/temp'
   
   def install
+    countries = ["IRAN", "SUDAN", "CUBA", "NORTH KOREA", "SYRIA", "DEMOCRATIC PEOPLE'S REPUBLIC OF KOREA"]
+    print "Enter your country: "
+    until !(country = STDIN.gets.chomp).empty?
+    end
+    if countries.include?country.upcase
+        puts "You are prohibited from downloading our software according to our software license agreement. If you feel you have received this message in error, please send an email to info@gopivotal.com"
+        exit 0
+    end
+    puts "\nDo you agree to terms and conditions in the Software License Agreement at link below?"
+    puts "http://gopivotal.com/pivotal-products/software-license-agreement"
+    print "[yes, no] : "
+    until !(agree = STDIN.gets.chomp).empty?
+    end
+    if agree.downcase != 'yes'
+        puts "\nIn order to install this software you need to agree to terms and conditions in the Software License Agreement."
+        exit 0
+    end
+
     # Remove Windows scripts
     rm_rf Dir['**/*.bat']
 
