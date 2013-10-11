8f849f19c8d4cb66c7d4d5a3881e0dacd7d27c7e
Kashyap Parikh <parikhkc@gmail.com>
2013-10-10 17:01:47 -0700

    Adding springxd

---
+++ springxd.rb
@@ -0,0 +1,44 @@
+require 'formula'
+
+class Springxd < Formula
+  homepage 'http://projects.spring.io/spring-xd/'
+  url 'http://repo.spring.io/simple/libs-milestone-local/org/springframework/xd/spring-xd/1.0.0.M3/spring-xd-1.0.0.M3.zip'
+  sha1 'ec8e1f5711ce86cdf216977627dadb948c4f7a99'
+  version "1.0.0.M3"
+  
+  depends_on 'redis' => :optional
+  depends_on 'rabbitmq' => :optional
+
+  def install
+    # Remove Windows scripts
+    rm_rf Dir['**/*.bat']
+
+    # Install files
+    libexec.install Dir['*']
+    bin.install_symlink Dir["#{libexec}/shell/bin/*"]
+    bin.install_symlink Dir["#{libexec}/xd/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+
+   Usage:
+    To start XD single node (runs in foreground): 
+    xd-singlenode
+
+    To start XD admin and container seperatly (all runs in foreground):
+    redis-server
+    xd-admin
+    xd-container
+
+    Start XD Shell:
+    xd-shell
+
+    Create your first stream by typing following in xd shell: 
+    xd:> stream create --definition "time | log" --name ticktock
+    
+    Documentation:
+       http://docs.spring.io/spring-xd/docs/1.0.0.M3/reference/html
+
+    EOS
+  end
+end
