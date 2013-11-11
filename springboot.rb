8fd4007753ab7195a51a64c0a3cacc134ac4eb86
Ben Hale <bhale@gopivotal.com>
2013-11-11 08:07:44 +0000

    Update springboot to 0.5.0.M6
    This change updates the springboot formula to 0.5.0.M6.

--- springboot.rb
+++ springboot.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Springboot < Formula
   homepage 'http://projects.spring.io/spring-boot/'
-  url 'https://repo.spring.io/milestone/org/springframework/boot/spring-boot-cli/0.5.0.M5/spring-boot-cli-0.5.0.M5-bin.tar.gz'
-  version '0.5.0.M5'
-  sha1 '5f2b6eddbed59bbb54bd8390b71586f1d9a9515f'
+  url 'https://repo.spring.io/milestone/org/springframework/boot/spring-boot-cli/0.5.0.M6/spring-boot-cli-0.5.0.M6-bin.tar.gz'
+  version '0.5.0.M6'
+  sha1 '5f6c2d6a99cb923191b00d3524f2b586db98e7e3'
 
   def install
     bin.install 'bin/spring'
-    lib.install 'lib/spring-boot-cli-0.5.0.M5.jar'
+    lib.install 'lib/spring-boot-cli-0.5.0.M6.jar'
     bash_completion.install 'bash_completion.d/spring'
   end
 end
