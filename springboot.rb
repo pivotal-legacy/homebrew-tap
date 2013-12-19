00ab21bdf743fa6ee7bf491ddaaf72b647d99c78
Phillip Webb <pwebb@gopivotal.com>
2013-12-19 01:01:42 -0800

    Update springboot to 0.5.0.M7

--- springboot.rb
+++ springboot.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
 class Springboot < Formula
-  homepage 'http://projects.spring.io/spring-boot/'
-  url 'https://repo.spring.io/milestone/org/springframework/boot/spring-boot-cli/0.5.0.M6/spring-boot-cli-0.5.0.M6-bin.tar.gz'
-  version '0.5.0.M6'
-  sha1 '5f6c2d6a99cb923191b00d3524f2b586db98e7e3'
+	homepage 'http://projects.spring.io/spring-boot/'
+	url 'https://repo.spring.io/milestone/org/springframework/boot/spring-boot-cli/0.5.0.M7/spring-boot-cli-0.5.0.M7-bin.tar.gz'
+	version '0.5.0.M7'
+	sha1 'c4a0ae98da62fbaaf96e3004e6982b74247b3ca0'
 
-  def install
-    bin.install 'bin/spring'
-    lib.install 'lib/spring-boot-cli-0.5.0.M6.jar'
-    bash_completion.install 'bash_completion.d/spring'
-  end
+	def install
+		bin.install 'bin/spring'
+		lib.install 'lib/spring-boot-cli-0.5.0.M7.jar'
+		bash_completion.install 'shell-completion/bash/spring'
+		zsh_completion.install 'shell-completion/zsh/_spring'
+	end
 end
