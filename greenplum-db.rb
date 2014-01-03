9bbdb7dddb7a73baa89f77d5aa7341a90d76882b
Kashyap Parikh <parikhkc@gmail.com>
2014-01-03 15:26:30 -0800

    Adding GPDB formula (beta)

---
+++ greenplum-db.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class GreenplumDb < Formula
+  homepage 'http://gopivotal.com/products/pivotal-greenplum-database'
+  url 'http://dist.vfabric.com.s3.amazonaws.com/greenplum-db-4.2.2.4.tgz'
+  sha1 '0e66a34da5bb361ebd917a737ec98483c64c4eae'
+  version "4.2.2.4"
+  
+  def install
+    # Install files
+    prefix.install Dir['*']
+    safe_system "ln -sf #{prefix} /usr/local/greenplum-db-4.2.2.4"
+    safe_system "ln -sf #{prefix} /usr/local/greenplum-db"
+  end
+
+  def caveats; <<-EOS.undent
+GreenplumDB formula is currently a beta version and UNSUPPORTED. Use it at your own risk.
+By installing, you agree to comply with the license at http://www.gopivotal.com/pivotal-products/software-license-agreement. If you disagree with these terms, please uninstall by typing "brew uninstall greenplum-db".
+
+To configure and start GreenplumDB please run following:
+    /usr/local/greenplum-db-4.2.2.4/install-gpdb.sh
+
+  EOS
+  end
+end
