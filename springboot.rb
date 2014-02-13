8814f15d24f3d8975a919dd8bef4956e90f97ce3
Phillip Webb <pwebb@gopivotal.com>
2014-02-12 20:46:21 -0800

    Update springboot to 1.0.0.RC2

--- springboot.rb
+++ springboot.rb
@@ -1,27 +1,27 @@
 require 'formula'
 
 class Springboot < Formula
-	homepage 'http://projects.spring.io/spring-boot/'
-	url 'https://repo.spring.io/milestone/org/springframework/boot/spring-boot-cli/1.0.0.RC1/spring-boot-cli-1.0.0.RC1-bin.tar.gz'
-	version '1.0.0.RC1'
-	sha1 'fcb6b29c550ce94b49da05453ea23b1fca29a691'
-	head 'https://github.com/spring-projects/spring-boot.git'
+  homepage 'http://projects.spring.io/spring-boot/'
+  url 'https://repo.spring.io/milestone/org/springframework/boot/spring-boot-cli/1.0.0.RC2/spring-boot-cli-1.0.0.RC2-bin.tar.gz'
+  version '1.0.0.RC2'
+  sha1 '0f7e9b7cd993e1d8592f4bea8401d204eba811c0'
+  head 'https://github.com/spring-projects/spring-boot.git'
 
-	if build.head?
-		depends_on 'maven' => :build
-	end
+  if build.head?
+    depends_on 'maven' => :build
+  end
 
-	def install
-		if build.head?
-			Dir.chdir('spring-boot-cli') { system 'mvn -U -DskipTests=true package' }
-			root = 'spring-boot-cli/target/spring-boot-cli-*-bin/spring-*'
-		else
-			root = '.'
-		end
+  def install
+    if build.head?
+      Dir.chdir('spring-boot-cli') { system 'mvn -U -DskipTests=true package' }
+      root = 'spring-boot-cli/target/spring-boot-cli-*-bin/spring-*'
+    else
+      root = '.'
+    end
 
-		bin.install Dir["#{root}/bin/spring"]
-		lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
-		bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
-		zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
-	end
+    bin.install Dir["#{root}/bin/spring"]
+    lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
+    bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
+    zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
+  end
 end
