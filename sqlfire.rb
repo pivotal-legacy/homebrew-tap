8c6dc61998a4189dbddb7e979e6b43ff7e7fb638
pivotalbuildguy <mbretl@gopivotal.com>
2014-02-19 16:33:27 -0800

    Update sqlfire.rb
    Update SQLFire brew dist to SQLFire 1.1.2

--- sqlfire.rb
+++ sqlfire.rb
@@ -1,29 +1,29 @@
 require 'formula'
 
 class Sqlfire < Formula
-  homepage 'http://gopivotal.com/pivotal-products/data/pivotal-sqlfire'
-  url 'http://bitcast-a.v1.sjc1.bitgravity.com/greenplum/SQLFire/1.1.1/vFabric_SQLFire_111_b42624.zip'
-  sha1 'eef7da9dc5ea53ffe8ed278cad44976b744b687c'
-  version "1.1.1"
+  homepage 'http://gopivotal.com/products/pivotal-sqlfire'
+  url 'http://download.pivotal.com.s3.amazonaws.com/sqlfire/1.1.2/Pivotal_SQLFire_112_b45732.zip'
+  sha1 '743280517fbf02bb7d38c1748a3a0b6b36cb9d7d'
+  version "1.1.2"
   
   def install
     # Remove Windows scripts
     rm_rf Dir['**/*.bat']
 
     # Install files
-    prefix.install %w{ EULA.txt lib/open_source_licenses-VMware_vFabric_SQLFire_1.1.1.txt}
+    prefix.install %w{ EULA.txt lib/open_source_licenses-Pivotal_SQLFire_1.1.2.txt}
     libexec.install Dir['*']
     bin.write_exec_script libexec/'bin/sqlf'
   end
 
   def caveats; <<-EOS.undent
-    By installing, you agree to comply with the license at http://gopivotal.com/pivotal-products/software-license-agreement. If you disagree with these terms, please uninstall by typing "brew uninstall sqlfire" in your terminal window.
+    By installing, you agree to comply with the license at http://gopivotal.com/products/software-license-agreement. If you disagree with these terms, please uninstall by typing "brew uninstall sqlfire" in your terminal window.
  
     Usage:
        sqlf
           
     Documentation:
-       https://www.vmware.com/support/pubs/vfabric-sqlfire.html
+       http://docs.gopivotal.com/sqlfire/index.html
 
     EOS
   end
