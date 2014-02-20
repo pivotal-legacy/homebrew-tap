07d33ff67d9a4d1584fa8a1701561526bd7521e0
pivotalbuildguy <mbretl@gopivotal.com>
2014-02-19 16:24:13 -0800

    Update gemfire.rb
    Update gemfire brew dist with GemFire 7.0.2

--- gemfire.rb
+++ gemfire.rb
@@ -1,17 +1,17 @@
 require 'formula'
 
 class Gemfire < Formula
-  homepage 'http://gopivotal.com/pivotal-products/data/pivotal-gemfire'
-  url 'http://bitcast-a.v1.sjc1.bitgravity.com/greenplum/GemFire/vFabric_GemFire_701_b40235.zip'
-  sha1 '03d1fe2ed5233e576f81a1d10270273d9f09d1f9'
-  version "7.0.1"
+  homepage 'http://gopivotal.com/products/pivotal-gemfire'
+  url 'http://downloads.cfapps.io/gemf_zip'
+  sha1 '69a138baa30e99d30fc98312e535481afd5c0ad8'
+  version "7.0.2"
   
   def install
     # Remove Windows scripts
     rm_rf Dir['**/*.bat']
 
     # Install files
-    prefix.install %w{ EULA.txt lib/open_source_licenses-VMware_vFabric_GemFire_7.0.1.txt}
+    prefix.install %w{ EULA.txt lib/open_source_licenses-Pivotal_GemFire_7.0.2.txt}
     libexec.install Dir['*']
     bin.write_exec_script libexec/'bin/agent'
     bin.write_exec_script libexec/'bin/cacheserver'
@@ -20,7 +20,7 @@ class Gemfire < Formula
   end
 
   def caveats; <<-EOS.undent
-    By installing, you agree to comply with the license at http://gopivotal.com/pivotal-products/software-license-agreement. If you disagree with these terms, please uninstall by typing "brew uninstall gemfire" in your terminal window.
+    By installing, you agree to comply with the license at http://gopivotal.com/products/software-license-agreement. If you disagree with these terms, please uninstall by typing "brew uninstall gemfire" in your terminal window.
 
     Usage:
        gfsh
@@ -29,7 +29,7 @@ class Gemfire < Formula
        agent
           
     Documentation:
-       https://www.vmware.com/support/pubs/vfabric-gemfire.html
+       http://docs.gopivotal.com/gemfire/index.html
 
     EOS
   end
