9c821f4b391064e7b6dfc57b217c636f9dc91004
Kashyap Parikh <parikhkc@gmail.com>
2014-03-03 12:19:14 -0800

    Fix caveats formatting

--- greenplum-db.rb
+++ greenplum-db.rb
@@ -9,17 +9,23 @@ class GreenplumDb < Formula
   def install
     # Install files
     prefix.install Dir['*']
-    safe_system "ln -sf #{prefix} /usr/local/greenplum-db-4.2.2.4"
+    safe_system "ln -sf #{prefix} /usr/local/greenplum-db-#{version}"
     safe_system "ln -sf #{prefix} /usr/local/greenplum-db"
   end
 
   def caveats; <<-EOS.undent
+
 GreenplumDB formula is currently a beta version and UNSUPPORTED. Use it at your own risk.
 By installing, you agree to comply with the license at http://www.gopivotal.com/pivotal-products/software-license-agreement. If you disagree with these terms, please uninstall by typing "brew uninstall greenplum-db".
 
 To configure and start GreenplumDB please run following:
-    /usr/local/greenplum-db-4.2.2.4/install-gpdb.sh
+    /usr/local/greenplum-db/install-gpdb.sh
 
-  EOS
+
+To stop and uninstall GreenplumDB please run following:
+    /usr/local/greenplum-db/stop-gpdb.sh
+    brew uninstall greenplum-db
+
+EOS
   end
 end
