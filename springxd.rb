2c8756dc65a2e89307cf9bc52da9164eddecde19
Kashyap Parikh <kparikh@gopivotal.com>
2014-04-15 22:14:11 -0700

    Update SpringXD to M6
    Added symlink to hsqldb/bin
    Updated usage to install and start distributed components

--- springxd.rb
+++ springxd.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Springxd < Formula
   homepage 'http://projects.spring.io/spring-xd/'
-  url 'http://deq44nzfzkcur.cloudfront.net/simple/libs-milestone-local/org/springframework/xd/spring-xd/1.0.0.M5/spring-xd-1.0.0.M5-dist.zip' , :using => :curl
-  sha1 '3d34600b24a2d52fc792ef448a0569a2c88bb8a5'
-  version "1.0.0.M5"
-  
+  url 'http://deq44nzfzkcur.cloudfront.net/simple/libs-milestone-local/org/springframework/xd/spring-xd/1.0.0.M6/spring-xd-1.0.0.M6-dist.zip' , :using => :curl
+  sha1 'b2231bac756581ea6d71ecacefb139b9c2f69fc1'
+  version "1.0.0.M6"
+
   depends_on 'redis' => :optional
   depends_on 'rabbitmq' => :optional
 
@@ -18,27 +18,32 @@ class Springxd < Formula
     bin.install_symlink Dir["#{libexec}/shell/bin/*"]
     bin.install_symlink Dir["#{libexec}/xd/bin/*"]
     bin.install_symlink Dir["#{libexec}/gemfire/bin/*"]
+    bin.install_symlink Dir["#{libexec}/hsqldb/bin/*"]
   end
 
   def caveats; <<-EOS.undent
 
    Usage:
-    To start XD single node (runs in foreground): 
-    xd-singlenode
+    To start XD single node (runs in foreground):
+        xd-singlenode
 
     To start XD admin and container separately (all run in foreground):
-    redis-server
-    xd-admin
-    xd-container
+        brew update && brew install redis
+        brew install zookeeper
+        redis-server
+        hsqldb-server
+        zkServer start
+        xd-admin
+        xd-container
 
     Start XD Shell:
-    xd-shell
+        xd-shell
 
-    Create your first stream by typing following in xd shell: 
-    xd:> stream create --definition "time | log" --name ticktock
-    
+    Create your first stream by typing following in xd shell:
+        xd:> stream create --definition "time | log" --name ticktock
+
     Documentation:
-       http://docs.spring.io/spring-xd/docs/1.0.0.M5/reference/html
+       http://docs.spring.io/spring-xd/docs/1.0.0.M6/reference/html
 
     EOS
   end
