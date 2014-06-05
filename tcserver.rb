0c16286b7e1b8ff9f7db9f5b9cfa98b9b3ad5ecc
Kashyap Parikh <kparikh@gopivotal.com>
2014-06-05 09:50:06 -0700

    Update to 2.9.6 developer edition

--- tcserver.rb
+++ tcserver.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tcserver < Formula
   homepage 'http://www.gopivotal.com/?q=pivotal-products/pivotal-application-cloud-fabric/pivotal-tc-server'
-  url 'http://download.pivotal.com.s3.amazonaws.com/tcserver/2.9.6/vfabric-tc-server-standard-2.9.6.RELEASE.tar.gz'
-  sha1 '1fed309cef7ff1a404a71ffc0a616734e08eb0ba'
+  url 'http://download.pivotal.com.s3.amazonaws.com/tcserver/2.9.6/vfabric-tc-server-developer-2.9.6.RELEASE.tar.gz'
+  sha1 '1cdb4e1bb195db97ee578b2755e7712453c2739e'
   version "2.9.6"
   
   # logs, lib and temp folder need to exist for base template to work
