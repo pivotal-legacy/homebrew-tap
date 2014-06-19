4ba14a3aec92bedda4286899221e4d6b8b78bea1
Anand Gaitonde <agaitonde@pivotallabs.com>
2014-06-19 16:10:58 -0700

    Updating to 6.2.0.

--- cloudfoundry-cli.rb
+++ cloudfoundry-cli.rb
@@ -3,9 +3,9 @@ require 'formula'
 class CloudfoundryCli < Formula
   homepage 'https://github.com/cloudfoundry/cli'
   head 'https://cli.run.pivotal.io/edge?arch=macosx64'
-  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.1.2'
-  version '6.1.2'
-  sha1 '19df87677b92129f01e339e1f8f043831b6168d3'
+  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.2.0&source=homebrew'
+  version '6.2.0'
+  sha1 '0e1dea612419ca9e9d6aae985c7c84cc6bb62a38'
 
   depends_on :arch => :x86_64
 
