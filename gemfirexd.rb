6451916d14e7bfa1ffb84a59e0c6eadcb1efc2b8
pivotalbuildguy <mbretl@gopivotal.com>
2014-12-01 14:17:35 -0800

    Update GemFire XD version to 1.3.1

--- gemfirexd.rb
+++ gemfirexd.rb
@@ -1,17 +1,17 @@
 require 'formula'
 
 class Gemfirexd < Formula
-  homepage 'http://www.pivotal.io/big-data/pivotal-hd'
-  url 'http://download.pivotal.com.s3.amazonaws.com/gemfirexd/1.3.0/Pivotal_GemFireXD_130_b48613_Linux.zip'
-  sha1 'bd2c6911f8a9b684a43e705e752e4c89cd0d2ef7'
-  version "1.3.0"
+  homepage 'http://www.pivotal.io/big-data/pivotal-gemfire-xd'
+  url 'http://download.pivotal.com.s3.amazonaws.com/gemfirexd/1.3.1/Pivotal_GemFireXD_131_b49833_Linux.zip'
+  sha1 '1ebf1cefd757363eb9512ffeb48b5a64d39095d8'
+  version "1.3.1"
   
   def install
     # Remove Windows scripts
     rm_rf Dir['**/*.bat']
 
     # Install files
-    prefix.install %w{ EULA.txt lib/open_source_licenses-Pivotal_GemFireXD_1.3.txt}
+    prefix.install %w{ EULA.txt lib/open_source_licenses-Pivotal_GemFireXD_1.3.1.txt}
     libexec.install Dir['*']
     bin.write_exec_script libexec/'bin/color'
     bin.write_exec_script libexec/'bin/dataextractloader'
