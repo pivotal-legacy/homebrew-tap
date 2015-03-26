7bae950a267b4a480c05a4e77c56b39c8d162187
Sabby Anandan <sabby.anandan@gmail.com>
2015-03-26 11:26:43 -0700

    Update to Spring XD 1.1.1 Release.

--- springxd.rb
+++ springxd.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Springxd < Formula
   homepage 'http://projects.spring.io/spring-xd/'
-  url 'http://repo.spring.io/libs-release-local/org/springframework/xd/spring-xd/1.1.0.RELEASE/spring-xd-1.1.0.RELEASE-dist.zip' , :using => :curl
-  sha1 '34a439f7449554e36a868d4312334ae59360fe5d'
-  version "1.1.0.RELEASE"
+  url 'http://repo.spring.io/release/org/springframework/xd/spring-xd/1.1.1.RELEASE/spring-xd-1.1.1.RELEASE-dist.zip' , :using => :curl
+  sha1 'ddbad07635f18818efdcddbf51d2a101f4b970be'
+  version "1.1.1.RELEASE"
 
   depends_on 'redis' => :optional
   depends_on 'rabbitmq' => :optional
@@ -44,7 +44,7 @@ class Springxd < Formula
         xd:> stream create --definition "time | log" --name ticktock
 
     Documentation:
-       http://docs.spring.io/spring-xd/docs/1.1.0.RELEASE/reference/html/
+       http://docs.spring.io/spring-xd/docs/1.1.1.RELEASE/reference/html/
 
     EOS
   end
