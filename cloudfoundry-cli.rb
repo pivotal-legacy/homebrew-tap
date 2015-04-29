1e04de915c4a34da7f23e90d4dcb2d8f4248cee3
Aidan Feldman <aidan.feldman@gmail.com>
2015-04-29 02:19:23 -0400

    fix SHA1 for CLI v6.11.2

--- cloudfoundry-cli.rb
+++ cloudfoundry-cli.rb
@@ -5,7 +5,7 @@ class CloudfoundryCli < Formula
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
   url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.11.2&source=homebrew'
   version '6.11.2'
-  sha1 'f4bd3687d9fd0f803cb574ecfe9bcd22f3c872d0'
+  sha1 'ec8c6bc1585d620fdf6c42f59acec5368bc3e4b9'
 
   depends_on :arch => :x86_64
 
