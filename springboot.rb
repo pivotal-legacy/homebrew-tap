c22f21f99b853d7ac6affcca167120038cf086d5
Phillip Webb <pwebb@pivotal.io>
2015-06-04 01:25:45 -0700

    Update Spring Boot to 1.2.4.RELEASE

--- springboot.rb
+++ springboot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Springboot < Formula
   homepage 'http://projects.spring.io/spring-boot/'
-  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/1.2.3.RELEASE/spring-boot-cli-1.2.3.RELEASE-bin.tar.gz'
-  version '1.2.3.RELEASE'
-  sha1 '3ef581529d4111e19c60c2899a840b4a9aaea433'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/1.2.4.RELEASE/spring-boot-cli-1.2.4.RELEASE-bin.tar.gz'
+  version '1.2.4.RELEASE'
+  sha1 '328e83fc142c5390ab098cce523ce0cc72a2fa68'
   head 'https://github.com/spring-projects/spring-boot.git'
 
   if build.head?
