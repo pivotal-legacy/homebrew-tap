28039508478d4f969bff01a4fa092fd3dfebc3da
Alice Wyan <alice@cloudcredo.com>
2015-06-09 17:45:47 +0100

    Update bosh-init

--- bosh-init.rb
+++ bosh-init.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class BoshInit < Formula
   homepage 'https://github.com/cloudfoundry/bosh-init'
-  url 'https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.29-darwin-amd64'
-  version '0.0.29'
-  sha1 '524833c21d8cfa4dcd9d0e4d7ececdad8b474d71'
+  url 'https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.53-darwin-amd64'
+  version '0.0.53'
+  sha1 'e05fdeeb1e8e467eb7ad2fc5b88c470c75e78371'
 
   depends_on :arch => :x86_64
 
   def install
-    bin.install 'bosh-init-0.0.29-darwin-amd64' => 'bosh-init'
+    bin.install 'bosh-init-0.0.53-darwin-amd64' => 'bosh-init'
   end
 
   test do
