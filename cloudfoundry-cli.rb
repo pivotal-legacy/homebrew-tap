43df080bf8e5555874466ab04c9ebe47c25b82e3
Paul Harris <paul@twoseat.com>
2015-07-03 10:33:07 +0100

    Update SHA1
    I believe the SHA1 is incorrect for the file that is downloaded. I modified my local .rb with '34083026a2b747ba176c71a3f2b12eb994b2b1c1' and /usr/local/bin/cf version 6.12.0-8c65bbd-2015-06-30T00:05:01+00:00 was downloaded and installed.

--- cloudfoundry-cli.rb
+++ cloudfoundry-cli.rb
@@ -5,7 +5,7 @@ class CloudfoundryCli < Formula
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
   url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.12.0&source=homebrew'
   version '6.12.0'
-  sha1 '8dfca97225499195b5ce9bbfb9f38f423fba78c0'
+  sha1 '34083026a2b747ba176c71a3f2b12eb994b2b1c1'
 
   depends_on :arch => :x86_64
 
