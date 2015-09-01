4db1abd15f59df61c5c95257917a27b8b5d0863f
Simon Leung <leungs@us.ibm.com>
2015-09-01 10:49:36 -0700

    Bump CLI to v6.12.3

--- cloudfoundry-cli.rb
+++ cloudfoundry-cli.rb
@@ -3,9 +3,9 @@ require 'formula'
 class CloudfoundryCli < Formula
   homepage 'https://github.com/cloudfoundry/cli'
   head 'https://cli.run.pivotal.io/edge?arch=macosx64&source=homebrew'
-  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.12.2&source=homebrew'
-  version '6.12.2'
-  sha1 '45aafd164562e61615febfe4d0df0c394eb58677'
+  url 'https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.12.3&source=homebrew'
+  version '6.12.3'
+  sha1 'd10c532ea711cd0d5549127e378dbc78c3971a92'
 
   depends_on :arch => :x86_64
 
