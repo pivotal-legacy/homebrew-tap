1754ac4d5369004401786a0b03e146e53d253cd6
Joseph Palermo <jpalermo@pivotal.io>
2015-10-22 14:41:54 -0700

    Update to latest version of bosh-init
    Signed-off-by: Abhi Hiremagalur <ahiremagalur@pivotal.io>

--- bosh-init.rb
+++ bosh-init.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class BoshInit < Formula
   homepage 'https://github.com/cloudfoundry/bosh-init'
-  url 'https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.53-darwin-amd64'
-  version '0.0.53'
-  sha1 'e05fdeeb1e8e467eb7ad2fc5b88c470c75e78371'
+  url 'https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.77-darwin-amd64'
+  version '0.0.77'
+  sha1 '5ccca7d2d65e6c83abe97f5c00fe0c9f778c9408'
 
   depends_on :arch => :x86_64
 
   def install
-    bin.install 'bosh-init-0.0.53-darwin-amd64' => 'bosh-init'
+    bin.install 'bosh-init-0.0.77-darwin-amd64' => 'bosh-init'
   end
 
   test do
