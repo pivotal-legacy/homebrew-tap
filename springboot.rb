cb76f8e778562f2e83413896a71b7f6f6347ccfc
Can Berk Güder <cbguder@pivotal.io>
2016-04-30 17:49:25 -0700

    Use SHA256 for Spring Boot

--- springboot.rb
+++ springboot.rb
@@ -4,7 +4,7 @@ class Springboot < Formula
   homepage 'http://projects.spring.io/spring-boot/'
   url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/1.3.3.RELEASE/spring-boot-cli-1.3.3.RELEASE-bin.tar.gz'
   version '1.3.3.RELEASE'
-  sha1 'c73d3ac8f7b654c1d74e3400b88d238170f9ee6f'
+  sha256 '601e52f71625d9cd5ce68fd4cab2aef58dc3ddc4d933f3f357aa20a3ef8ec754'
   head 'https://github.com/spring-projects/spring-boot.git'
 
   if build.head?
