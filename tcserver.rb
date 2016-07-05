91e26ac6ae2123b925def7dcba64fcfdcb1381c0
Jason Konicki <jkonicki@gopivotal.com>
2016-07-05 14:47:53 -0400

    Update version to 3.1.5

--- tcserver.rb
+++ tcserver.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Tcserver < Formula
   homepage 'http://tcserver.docs.pivotal.io/index.html'
-  url 'http://public.pivotal.com.s3.amazonaws.com/releases/tcserver/3.1.4.RELEASE/tcserver-3.1.4.RELEASE-developer.tar.gz'
-  sha1 'a9a4a13477c732837e31702c78fa80c76028b89f'
-  version "3.1.4"
+  url 'http://public.pivotal.com.s3.amazonaws.com/releases/tcserver/3.1.5.RELEASE/tcserver-3.1.5.RELEASE-developer.tar.gz'
+  sha1 '8a1607e06d523cd2a13ed2ef429cfe494572b762'
+  version "3.1.5"
   
   # logs, lib and temp folder need to exist for base template to work
   skip_clean 'libexec/templates/base/logs'
@@ -16,7 +16,7 @@ class Tcserver < Formula
     rm_rf Dir['**/*.bat']
 
     # Install files
-    prefix.install %w{ README.txt licenses/Pivotal_EULA.txt licenses/open-source-licenses.txt}
+    prefix.install %w{ README.txt licenses/Pivotal_EULA.txt }
     libexec.install Dir['*']
     bin.install_symlink Dir["#{libexec}/*.sh"]
     
@@ -34,13 +34,16 @@ class Tcserver < Formula
           tcruntime-instance.sh create myinstance
           
        To create a new tc Server instance with Spring Insight monitoring:
-          tcruntime-instance.sh create -t insight myinstance 
+          tcruntime-instance.sh create myinstance -t insight 
        
        To control tc Server instance (in current directory):
           tcruntime-ctl.sh myinstance start
           
     Documentation:
        http://tcserver.docs.pivotal.io/
+    
+    Open Source License:
+       https://network.pivotal.io/open-source
 
     For inquiries about commercial licensing, support, training, and consulting, please contact us at tcserver@pivotal.io
     EOS
