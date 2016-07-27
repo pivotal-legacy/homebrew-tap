9121476d2857669a1db8fc936635cd6d30ca90d5
Sunrise Concourse CI <concourse-ci@sunrise.ci.cf-app.com>
2016-07-27 01:48:20 +0000

    Update pivnet CLI to version 0.0.18

--- pivnet-cli.rb
+++ pivnet-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class PivnetCli < Formula
   homepage 'https://github.com/pivotal-cf-experimental/go-pivnet'
-  version "0.0.17"
+  version "0.0.18"
   url "https://github.com/pivotal-cf-experimental/go-pivnet/releases/download/v#{version}/pivnet-darwin-amd64-#{version}"
-  sha256 "3ce86c406ad12b5d9ed9d1d410da60f27ed3a93e5c81fbc78ede987b95504a4a"
+  sha256 "a5c5d4ffd952497bdf9b05f71b16e0b988d947f436fba854b85993432fa5551e"
 
   depends_on :arch => :x86_64
 
