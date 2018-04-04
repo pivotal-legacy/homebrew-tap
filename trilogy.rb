30fc1b1777a760b811606ec6e152c92293275955
Konstantin Semenov <ksemenov@pivotal.io>
2018-04-04 20:20:00 +0100

    Add a formula for trilogy
    [#121746839]

---
+++ trilogy.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Trilogy < Formula
+  homepage 'https://github.com/pivotal/trilogy'
+  version "0.3.1"
+  url "https://github.com/pivotal/trilogy/releases/download/v#{version}/trilogy-#{version}-mac-linux.tgz"
+  sha256 "571bc3ab419bb295c9072fa8be2ec731edf6c3f292a011885fb45fdac9d53e46"
+
+  depends_on :java => "1.8"
+
+  def install
+    bin.install "trilogy"
+    bin.install "trilogy.jar"
+  end
+
+  test do
+    system "#{bin}/trilogy --help"
+  end
+end
