eebf6d0cce7c75d4f0da5a1c0224257bae5875f2
Bradford D. Boyle <bboyle@pivotal.io>
2018-04-17 12:15:01 -0700

    Calling 'depends_on ... => :run' is deprecated

--- git-together.rb
+++ git-together.rb
@@ -7,7 +7,7 @@ class GitTogether < Formula
   url "https://github.com/kejadlen/git-together/releases/download/v#{VERSION}/git-together-v#{VERSION}-x86_64-apple-darwin.tar.gz"
   sha256 "072ad72d40c6771d172b12c6e5cb47fcc4190ea5b72faa99025774c675b15a13"
 
-  depends_on "libssh2" => :run
+  depends_on "libssh2"
 
   def install
     bin.install "git-together"
