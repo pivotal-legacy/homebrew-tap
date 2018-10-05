1accffb60525e209849997051d78341bd2fbb3ec
Xin Zhang <xzhang@pivotal.io>
2018-10-04 19:22:40 -0500

    Bump to v1.0.2 with bette template support
    Authored-by: Xin Zhang <xzhang@pivotal.io>

--- git-author.rb
+++ git-author.rb
@@ -1,21 +1,19 @@
 class GitAuthor < Formula
+  desc "The git-author to use git template to include multiple contributors"
   homepage "https://github.com/pivotal/git-author"
-  url "https://github.com/pivotal/git-author/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "68c88a12dd03f55d078fef2235bd0835f2378b08e625cc2998948fa44a202cd6"
-
-  depends_on "git-together" => :install
+  url "https://github.com/pivotal/git-author/archive/v1.0.2.tar.gz"
+  depends_on "git-together"
 
   def install
-    system "touch ~/.git-author-template"
-    system "git config --global commit.template ~/.git-author-template"
     bin.install "git-author"
+    bin.install "setup.sh"
+    system "#{bin}/setup.sh || true"
   end
 
-  def caveats; <<~EOS
+  def caveats; <<-EOS
     export GIT_TOGETHER_NO_SIGNOFF=1 to ~/.bash_profile
     to disable --signoff added by git-together commit
-    EOS
+  EOS
   end
 
   test do
