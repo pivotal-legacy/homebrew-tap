403ebc86207626320ab177253476c9a26eacf4e3
Peter Blum <pblum@pivotal.io>
2018-11-11 08:16:04 -0600

    bump PACE cli version to v0.6

--- pace-cli.rb
+++ pace-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class PaceCli < Formula
   homepage 'https://github.com/Pivotal-Field-Engineering/pace-builder'
-  version "0.5"
+  version "0.6"
   url "https://github.com/Pivotal-Field-Engineering/pace-builder/releases/download/v#{version}/pace-darwin"
-  sha256 "9e01af572908b91c767607b89460efec040d2793e0c96e948dfff14314f48a2f"
+  sha256 "9eab7cd04546b578c90e03f0d9b58e9e0977d6b4ae10df8ea0c1634514cddec8"
 
   depends_on :arch => :x86_64
 
