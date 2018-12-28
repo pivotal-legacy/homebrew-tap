ce15e20393e888a4d99d3c033428e12c8dd3ce17
Konstantin Semenov <jhvhs@users.noreply.github.com>
2018-12-28 16:13:51 +0000

    Bump trilogy to 0.3.2

--- trilogy.rb
+++ trilogy.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Trilogy < Formula
   homepage 'https://github.com/pivotal/trilogy'
-  version "0.3.1"
+  version "0.3.2"
   url "https://github.com/pivotal/trilogy/releases/download/v#{version}/trilogy-#{version}-mac-linux.tgz"
   sha256 "571bc3ab419bb295c9072fa8be2ec731edf6c3f292a011885fb45fdac9d53e46"
 
