a6888063d423b03964c0f7bc705da89d51b0758e
Konstantin Semenov <jhvhs@users.noreply.github.com>
2018-12-28 16:18:44 +0000

    Update the SHA1 as well

--- trilogy.rb
+++ trilogy.rb
@@ -4,7 +4,7 @@ class Trilogy < Formula
   homepage 'https://github.com/pivotal/trilogy'
   version "0.3.2"
   url "https://github.com/pivotal/trilogy/releases/download/v#{version}/trilogy-#{version}-mac-linux.tgz"
-  sha256 "571bc3ab419bb295c9072fa8be2ec731edf6c3f292a011885fb45fdac9d53e46"
+  sha256 "1c6f7039da26f0f909cf4daf7b73c9a91f2241c4108a1cdeb072fb5b3732378e"
 
   depends_on :java => "1.8"
 
