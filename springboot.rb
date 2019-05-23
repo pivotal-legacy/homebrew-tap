cf89c666afac0be3f185e765adb5b37d081bd650
Phillip Webb <pwebb@pivotal.io>
2019-05-23 13:46:12 -0700

    Upgrade to Spring Boot 2.1.5.RELEASE

--- springboot.rb
+++ springboot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Springboot < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.1.4.RELEASE/spring-boot-cli-2.1.4.RELEASE-bin.tar.gz'
-  version '2.1.4.RELEASE'
-  sha256 '73a6351eb47f741ad8f1b5ab98051cc1b7ecdcb912bcd437b7e5c0618f585ce9'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.1.5.RELEASE/spring-boot-cli-2.1.5.RELEASE-bin.tar.gz'
+  version '2.1.5.RELEASE'
+  sha256 'c4d07caaae149059e70a0096d61be3c46f73f0c4590277dd8006fd3decbba50e'
   head 'https://github.com/spring-projects/spring-boot.git'
 
   if build.head?
