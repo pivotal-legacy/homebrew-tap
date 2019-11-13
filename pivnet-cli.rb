ad95d96ab62f30681447017657f2baa6a3c966ab
Pivnet-CLI CI <concourse-ci@pivnet-resource.ci.cf-app.com>
2019-11-13 20:33:11 +0000

    Update pivnet CLI to version 0.0.74

--- pivnet-cli.rb
+++ pivnet-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class PivnetCli < Formula
   homepage 'https://github.com/pivotal-cf/pivnet-cli'
-  version "0.0.73"
+  version "0.0.74"
   url "https://github.com/pivotal-cf/pivnet-cli/releases/download/v#{version}/pivnet-darwin-amd64-#{version}"
-  sha256 "9ecf325de10d619da9629749d9316d5d3afd892e72a0d9ac1e1e7a60e61b7008"
+  sha256 "d4d50ea398cb496afb6b9bfde67ba5404762de34027fb8d3c13ac692a9dbda0a"
 
   depends_on :arch => :x86_64
 
