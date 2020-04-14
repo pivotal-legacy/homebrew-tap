846af635b12c0006fedbb23e9e75b038b6cc93c8
Wes Schlichter <wschlichter@vmware.com>
2020-04-14 13:54:34 -0500

    Update to tc Server 4.1.0

--- tcserver.rb
+++ tcserver.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Tcserver < Formula
   homepage 'http://tcserver.docs.pivotal.io/index.html'
-  url 'https://s3.amazonaws.com/public.pivotal.com/releases/tcserver/4.0.11.RELEASE/tcserver-4.0.11.RELEASE-developer.tar.gz'
-  sha256 '9bafa17387075815f5e929937878a6d3c8158993e0e16fdc6b0ed1800aed9e33'
-  version '4.0.11'
+  url 'https://s3.amazonaws.com/public.pivotal.com/releases/tcserver/4.1.0.RELEASE/tcserver-4.1.0.RELEASE-developer.tar.gz'
+  sha256 '4c8a8dd699d0d7681aefd289bc448c2636cd989242e5bdf344fa3ec22e58833d'
+  version '4.1.0'
 
   def install
     rm_rf Dir['**/*.bat']
