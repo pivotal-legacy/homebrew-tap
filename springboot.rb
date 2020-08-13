3344f64c5c1bdeeb868ecfd782f29da2191bd592
Andy Wilkinson <awilkinson@pivotal.io>
2020-08-13 18:26:18 +0100

    Upgrade to Spring Boot 2.3.3.RELEASE

--- springboot.rb
+++ springboot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Springboot < Formula
   homepage 'https://spring.io/projects/spring-boot'
-  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.3.2.RELEASE/spring-boot-cli-2.3.2.RELEASE-bin.tar.gz'
-  version '2.3.2.RELEASE'
-  sha256 '19f24c4e6838b430d89d1a8acf92e59e4167a1682d40edbe95a863359be9c79a'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.3.3.RELEASE/spring-boot-cli-2.3.3.RELEASE-bin.tar.gz'
+  version '2.3.3.RELEASE'
+  sha256 'fc879de6446bd652a2c8e79184d81732b5e62333994ee51947d3631ed4deb71c'
   head 'https://github.com/spring-projects/spring-boot.git'
 
   if build.head?
