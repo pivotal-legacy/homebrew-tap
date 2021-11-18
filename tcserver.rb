770d97abb8f2e073e5622bb5df24f253982b6bfd
Wes Schlichter <wesschlichter@users.noreply.github.com>
2021-11-18 15:13:11 -0600

    Update tcsever.b
    This update corrects the sha256 calculation

--- tcserver.rb
+++ tcserver.rb
@@ -8,7 +8,7 @@ class Tcserver < Formula
   # brew needs something to download and since the bits are no longer available this allows that process to pass 
   # allowing the message to be presented to the user
   url 'https://github.com/pivotal-legacy/homebrew-tap/blob/9b7682ca3281b400b79bad71102d60c3871ead12/README-tcserver.md'
-  sha256 '7342774fb3828724bff334b67a070459376fa5e13519a8c3567ca0e12902faae'
+  sha256 '69775ef07b286d6deb83f1f24e9c016a39c9ce7ebab828398a50f1e4568b20aa'
   # This causes brew to update to show the error message.
   version '4.1.9999'
 
