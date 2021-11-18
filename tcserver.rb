329482a1e807d37b76a53c3f2c73798c9c4b64d4
Wes Schlichter <wesschlichter@users.noreply.github.com>
2021-11-18 13:25:38 -0600

    Update tcserver.rb to decommission homebrew tap
    This change will cause brew to error out upon upgrade command being ran. Displaying message to user

--- tcserver.rb
+++ tcserver.rb
@@ -1,44 +1,21 @@
 require 'formula'
 
+# The tc Server homebrew tap has been decommissioned. 
+# Please visit https://network.tanzu.vmware.com/ to download
+
 class Tcserver < Formula
   homepage 'http://tcserver.docs.pivotal.io/index.html'
-  url 'https://s3.amazonaws.com/public.pivotal.com/releases/tcserver/4.1.11.RELEASE/tcserver-4.1.11.RELEASE-developer.tar.gz'
-  sha256 'b4235156daf2b0d1032ee0d8dc693836beecacc68b0a97f4cafb204ed5e414fb'
-  version '4.1.11'
+  # brew needs something to download and since the bits are no longer available this allows that process to pass 
+  # allowing the message to be presented to the user
+  url 'http://tcserver.docs.pivotal.io/index.html'
+  sha256 'df2c8b64b2e3e06a060f76f1f0f7bf9e668040e19324533a24e19a0976dab8b7'
+  # This causes brew to update to show the error message.
+  version '4.1.9999'
 
   def install
-    rm_rf Dir['**/*.bat']
-    distDir = "developer-#{version}.RELEASE"
-    %w(README.txt licenses/VMware_EULA.txt).each do |readme|
-      prefix.install ["#{distDir}/#{readme}"]
-    end
-    libexec.install Dir['*']
-    bin.install_symlink Dir["#{libexec}/#{distDir}/tcserver"]
-    tcsInstancesDir = var.join('tcserver').join('instances')
-    tcsInstancesDir.mkpath
-    IO.write("#{libexec}/#{distDir}/conf/tcserver.properties",
-             "instances.directory=#{tcsInstancesDir}\n", mode: 'a')
-  end
+    ohai "tc Server tap is no longer available!"
 
-  def caveats; <<-EOS
-    By installing, you agree to comply with the license at https://network.pivotal.io/legal_documents/vmware_eula.
-    If you disagree with these terms, please uninstall by typing "brew uninstall tcserver" in your terminal window.
+    odie "Please download and install from Tanzu Network https://network.tanzu.vmware.com/"
 
-    Usage:
-       To create a new tc Server instance:
-          > tcserver create myinstance
-
-       To control tc Server instance:
-          > tcserver start|stop|restart|status myinstance
-
-    Documentation:
-       https://tcserver.docs.pivotal.io/
-
-    Open Source License:
-       https://network.pivotal.io/open-source
-
-    For inquiries about commercial licensing, support, training, and consulting, please contact us at:
-       mailto:tcserver@pivotal.io
-    EOS
   end
 end
