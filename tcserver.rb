f43b81e1d05351574d9919dfb947257726d69cce
Wes Schlichter <wesschlichter@users.noreply.github.com>
2021-11-18 15:05:34 -0600

    Update tcserver.rb

--- tcserver.rb
+++ tcserver.rb
@@ -7,7 +7,7 @@ class Tcserver < Formula
   homepage 'http://tcserver.docs.pivotal.io/index.html'
   # brew needs something to download and since the bits are no longer available this allows that process to pass 
   # allowing the message to be presented to the user
-  url 'https://github.com/wesschlichter/homebrew-tap/blob/4081dc9d409f181835876dc723a7275b7a42b390/README-tcserver.md'
+  url 'https://github.com/pivotal-legacy/homebrew-tap/blob/9b7682ca3281b400b79bad71102d60c3871ead12/README-tcserver.md'
   sha256 '7342774fb3828724bff334b67a070459376fa5e13519a8c3567ca0e12902faae'
   # This causes brew to update to show the error message.
   version '4.1.9999'
