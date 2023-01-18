4c927c3ad75990358d16bad2031bd338c444250e
Grant Hutchins <grant.hutchins@strongholdim.com>
2023-01-18 11:57:02 -0600

    Point git-author to updated vmware-archive repo
    Also, mark it as deprecated.
    
    Authored-by: Grant Hutchins <grant.hutchins@strongholdim.com>

--- git-author.rb
+++ git-author.rb
@@ -1,9 +1,11 @@
 class GitAuthor < Formula
   desc "The git-author to use git template to include multiple contributors"
-  homepage "https://github.com/pivotal/git-author"
-  url "https://github.com/pivotal/git-author/archive/v1.1.0.tar.gz"
+  homepage "https://github.com/vmware-archive/git-author"
+  url "https://github.com/vmware-archive/git-author/archive/v1.1.0.tar.gz"
   sha256 "603bfd9376b2ce7555dd5b29770c07d5ca73a06fc607f1ee842c371dc9357b9d"
   depends_on "git-together"
+
+  deprecate! date: "2023-01-18", because: :repo_archived
 
   def install
     bin.install "git-author"
@@ -11,9 +13,8 @@ class GitAuthor < Formula
     system "#{bin}/setup.sh || true"
   end
 
-  def caveats; <<-EOS
-    Please follow https://github.com/pivotal/git-author#setup for further setup instructions.
-  EOS
+  def caveats
+    "Please follow https://github.com/vmware-archive/git-author#setup for further setup instructions."
   end
 
   test do
